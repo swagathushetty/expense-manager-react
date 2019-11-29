26a24e6a4317cee811f740ebd0e21efb31fc53e1
Uday Shetty <udayshetty@Udays-MacBook-Air.local>
2019-11-30 00:17:56 +0530

    added a Summary functionality

--- src/components/ExpenseDashBoardPage.js
+++ src/components/ExpenseDashBoardPage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ExpenseList from './ExpenseList'
 import ExpenseListFilters from './ExpenseListFilters'
-import ExpenseListItem from './ExpenseListItem'
-
+import ExpensesSummary from './ExpensesSummary'
 
 const ExpenseDasBoardPage = () => {
     return (
         <div>
+             <ExpensesSummary />
             <ExpenseListFilters />
             <ExpenseList />
        </div>
