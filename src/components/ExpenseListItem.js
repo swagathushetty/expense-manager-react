0e4b5ef48375424d00c5b1f0f065fe4de0f6febc
Uday Shetty <udayshetty@Udays-MacBook-Air.local>
2019-11-29 14:48:41 +0530

    refactored amount and date

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from'react-redux'
-import {removeExpense} from '../actions/expenses'
+import moment from 'moment'
+import numeral from 'numeral'
+numeral.locale('inr');
 
 //before we used props as argument
 //we did props.expenses.amount etc
@@ -11,9 +13,29 @@ export const ExpenseListItem=({id,description,amount,createdAt})=>{
         <Link to={`/edit/${id}`}>
                 <h3>{description}</h3>
         </Link>
-           <p>{amount}-{createdAt}</p>
+           <p>
+            {numeral(amount/100).format('$0,0.00')}
+           -
+           {moment(createdAt).format('MMMM Do,YYYY')}
+           </p>
         </div>
     )
 }
 
 export default connect()(ExpenseListItem)
+
+numeral.register('locale', 'inr', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    currency: {
+        symbol: '₹'
+    }
+});
