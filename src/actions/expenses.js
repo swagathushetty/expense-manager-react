3f4d42e41b1784cdea8b16a7cca62770eeb5b790
Uday Shetty <udayshetty@Udays-MacBook-Air.local>
2019-12-09 14:06:16 +0530

    added remove functionality from firebae

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -37,6 +37,16 @@ export const removeExpense = ({ id } = {}) => {
     }
 }
 
+//s
+export const startRemoveExpense=({id}={})=>{
+    return (dispatch)=>{
+        return database.ref(`expenses/${id}`).remove().then(()=>{
+            dispatch(removeExpense({id}))
+        })
+    }
+
+}
+
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => {
     return {
